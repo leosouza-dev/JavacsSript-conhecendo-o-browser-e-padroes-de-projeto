d8c0eff6c2d6c90cd7ddd36c6b08458c87deb73d
leosouza-dev <sarival@sarival.com.br>
2020-06-22 12:17:15 -0300

    construtor e método obtemVolume

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -1,9 +1,9 @@
 class Negociacao{
 
-    constructor(){
-        
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor){
+
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
 }
