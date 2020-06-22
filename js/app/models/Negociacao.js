9f727c3e4f7724422ec823ba56aba2a87f3d89ac
leosouza-dev <sarival@sarival.com.br>
2020-06-22 13:41:25 -0300

    encapsulamento usando underline e metodo get

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -2,8 +2,24 @@ class Negociacao{
 
     constructor(data, quantidade, valor){
 
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    getData(){
+        return this._data;
+    }
+
+    getQuantidade(){
+        return this._quantidade;
+    }
+
+    getValor(){
+        return this._valor;
+    }
+
+    getVolume(){
+        return this._quantidade * this._valor;
     }
 }
