fe530794854ba80c3dcb13132b85fd41b70a9076
leosouza-dev <sarival@sarival.com.br>
2020-06-23 19:17:30 -0300

    Evitando percorrer o DOM muitas vezes

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,19 +1,22 @@
 class NegociacaoController{
 
+    constructor(){
+
+        // acessa o dom uma unica vez
+        // antes estavamos acessando sempre que clicava no botão
+        let $ = document.querySelector.bind(document);
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+    
     adiciona(event){
         event.preventDefault();
 
-        // criando um alias - apelido para essa grande função
-        // bind(document) - mantem associacao do querySelector com o this (document)
-        let $ = document.querySelector.bind(document);
-        let inputData = $('#data')
-        let inputQuantidade = $('#quantidade')
-        let inputValor = $('#valor')
-
         // exibe yyyy/mm/dd
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
         
     }
 }
