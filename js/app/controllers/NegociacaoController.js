1e0a367e850fb0d47919f8e7edd02df4db8dbd49
leosouza-dev <sarival@sarival.com.br>
2020-06-23 18:57:59 -0300

    teste - exibindo valores do input

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -2,6 +2,14 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
-        alert('chamer acao no controller');
+
+        let inputData = document.querySelector('#data')
+        let inputQuantidade = document.querySelector('#quantidade')
+        let inputValor = document.querySelector('#valor')
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+        
     }
 }
