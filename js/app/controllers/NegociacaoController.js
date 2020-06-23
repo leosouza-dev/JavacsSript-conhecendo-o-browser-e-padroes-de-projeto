5e753687c33abefc3ae2b01b6b537e374edad8bf
leosouza-dev <sarival@sarival.com.br>
2020-06-23 19:09:45 -0300

    criando alias para queryselector

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -3,10 +3,14 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        let inputData = document.querySelector('#data')
-        let inputQuantidade = document.querySelector('#quantidade')
-        let inputValor = document.querySelector('#valor')
+        // criando um alias - apelido para essa grande função
+        // bind(document) - mantem associacao do querySelector com o this (document)
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data')
+        let inputQuantidade = $('#quantidade')
+        let inputValor = $('#valor')
 
+        // exibe yyyy/mm/dd
         console.log(inputData.value);
         console.log(inputQuantidade.value);
         console.log(inputValor.value);
