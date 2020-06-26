3792b8ff218e5b7fe736f35104cf1967266c8f77
leosouza-dev <sarival@sarival.com.br>
2020-06-26 17:03:42 -0300

    Tornando metodos estaticos

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -13,18 +13,16 @@ class NegociacaoController{
     adiciona(event){
 
         event.preventDefault();
-
-        let helper = new DateHelper();
     
         let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         // testando
         console.log(negociacao);
-        console.log(helper.dataParaTexto(negociacao.data));
+        console.log(DateHelper.dataParaTexto(negociacao.data));
         
         // todo - add negociacao em uma lista
     }
