58a1bb6be6ed58e1d78ef0221eac4cc9316d5701
leosouza-dev <sarival@sarival.com.br>
2020-06-26 17:22:19 -0300

    Template string

--- js/app/helper/DateHelper.js
+++ js/app/helper/DateHelper.js
@@ -6,9 +6,8 @@ class DateHelper {
 
     static dataParaTexto(data) {
 
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+
       }
 
     static textoParaData(texto) {
